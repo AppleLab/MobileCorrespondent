6f628fbbdffde4ede80163fc0e58c56bf52fa9ac
Михаил <mihail@MacBook-Air-Mihail.local>
2014-07-05 10:43:42 +0400

    ver 1.1.2

---
+++ MobCor/NewsAdd.h
@@ -0,0 +1,13 @@
+//
+//  NewsAdd.h
+//  MobCor
+//
+//  Created by Михаил on 05.07.14.
+//  Copyright (c) 2014 kpfuitisios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsAdd : UIViewController
+
+@end
---
+++ MobCor/NewsAdd.m
@@ -0,0 +1,49 @@
+//
+//  NewsAdd.m
+//  MobCor
+//
+//  Created by Михаил on 05.07.14.
+//  Copyright (c) 2014 kpfuitisios. All rights reserved.
+//
+
+#import "NewsAdd.h"
+
+@interface NewsAdd ()
+
+@end
+
+@implementation NewsAdd
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
